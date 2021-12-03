a9375fbd01ed200c75b7ad595bdc96979acb9a15
Suryadevelops <suryadevelops@gmail.com>
2021-12-03 12:32:59 +0000

    Navbar push

---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../Styles/Navbar.css'
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            <div className="NavbarContainer">
+                <div className="Nav-Logo">
+                    RobinHood
+                    <i class="fas fa-feather-alt"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
